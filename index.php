18fb6088b3a9980ede85e4821634700b653cc9f5
Michel Soares Tartas <michel_s_t@yahoo.com.br>
2012-10-14 16:42:47 -0300

    Adicionada a index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<<<<<<< HEAD
+
+=======
+>>>>>>> 2ac77214152ee629f063b4f568eef16921bf36f3
+<head>
+    <title>
+		Trabalho 0
+    </title>
+</head>
+<body>
+	<h1>Michel Soares Tartas</h1>
+	<table border=1>
+		<caption>
+			Interesses em Tecnologias ao Longo do Curso
+		</caption>
+		<thead>
+			<tr>
+				</th><th>Nome</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Linguagem de programação Orientada a Objetos</td>
+			</tr>
+			<tr>
+				<tr></tr><td>Engines</td>
+			</tr>
+			<tr>
+				<tr></tr><td>Linguagem de estilos CSS</td>
+			</tr>
+			<tr>
+				<tr></tr><td>Banco de dados MySQL</td>
+			</tr>
+			<tr>
+				<tr></tr><td>Sistema de controle de versão Git</td>
+			</tr>
+			<tr>
+				<tr></tr><td>Linguagem de marcação HTML</td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+
+</html>
