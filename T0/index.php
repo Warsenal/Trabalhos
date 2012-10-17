2a740cffa53a426cc6e617aa5e30a39782705843
Michel Soares Tartas <michel_s_t@yahoo.com.br>
2012-10-17 18:02:36 -0300

    index

--- T0/index.php
+++ T0/index.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt-br" lang="pt-br"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<head>
-		<meta charset="UTF-8">
 		<title>Trabalho 0</title>
 		<link rel="stylesheet" type="text/css" href="style.css"/>
 	</head>
