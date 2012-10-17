5b69d383a8d7da5ba7358c72a6ea1c6063a5d697
Michel Soares Tartas <michel_s_t@yahoo.com.br>
2012-10-17 15:17:50 -0300

    Pronto!

--- T0/index.php
+++ T0/index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Trabalho 0</title>
 		<link rel="stylesheet" type="text/css" href="style.css"/>
 	</head>
