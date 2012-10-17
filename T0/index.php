6e60e4d6fa09f75e0f8fbcf7651e5fd37b92c395
Michel Soares Tartas <michel_s_t@yahoo.com.br>
2012-10-17 15:13:15 -0300

    Adicionado a linguagem

--- T0/index.php
+++ T0/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Trabalho 0</title>
 		<link rel="stylesheet" type="text/css" href="style.css"/>
 	</head>
