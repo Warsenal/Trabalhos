695d9d58763a07bbd9bdc3045c1cfed5401e2180
Michel Soares Tartas <michel_s_t@yahoo.com.br>
2012-10-17 14:18:44 -0300

    Arrumando erros

--- T0/index.php
+++ T0/index.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<!DOCTYPE html>
+<html>
 	<head>
 		<title>Trabalho 0</title>
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="style.css"/>
 	</head>
 	
 	<body>
-		<hr>
+		<hr/>
 		<h1>Michel Soares Tartas</h1>
 		<h3>Acadêmico da UFFS - Ciência da Computação</h3>
 		<h4><a href="mailto:michel_s_t@yahoo.com.br">michel_s_t@yahoo.com.br</a></h4>
-		<hr>
+		<hr/>
 		
 		<table align="center" border="1">
 			<th>Componente Curricular</th>
